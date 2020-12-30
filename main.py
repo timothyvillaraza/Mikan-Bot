e35bd2f31e84b5de9f3755bad5c9e8eda3f0db29
mafutofu <TimothyVillaraza@outlook.com>
2020-12-30 10:10:29 +0000

    Frequency map for a single user working (tested on a single user)

--- main.py
+++ main.py
@@ -3,72 +3,113 @@ import discord
 from discord.ext import commands
 from collections import defaultdict
 
+# Client Instance: All commands will start with '.'
+client = commands.Bot(command_prefix='.')
+
+
 # Class Definitions
-class word_frequency:
+class FrequencyMap:
     def __init__(self, name):
         self.name = name
         self.wordFreq = defaultdict(int)
         self.sortedKeys = None
 
+
 ############################
 # Word Frequency Functions #
 ############################
+
 
 #
 # Generate Word Frequency
 #
 # Take each word in the input and count up it's occurances
 #
-def wordFreqKeys(userInput):
+def generateWordFrequency(author, userInput):
     # defaultdict(int): If a key doesn't exist, add it with a default value of 0
-    wordFreq = defaultdict(int)
+    wordFreq = author.wordFreq
     for word in userInput.casefold().split():
         wordFreq[word] += 1
 
     # sorted() returns a *list* of key from most freq keys to least freq keys
     sorted_wordFreqKeys = sorted(wordFreq, key=wordFreq.get, reverse=True)
 
-    return wordFreq, sorted_wordFreqKeys
+    return author, wordFreq, sorted_wordFreqKeys
 
 
 #
 # Print Word Frequency
 #
-def printWordFreq(wordFreq, sortedKeys):
-    print("\n   [Word Count]")
-    for currentKey in sortedKeys:
-        print("   {}: {}".format(currentKey, wordFreq[currentKey]))
-
+def printWordFreq(user):
+    print(f'[Word Count for {user.name}]')
+    for currentKey in user.sortedKeys:
+        print(f"{currentKey}: {user.wordFreq[currentKey]}")
+    
     return
 
 
-# Client Instance: All commands will start with '.'
-client = commands.Bot(command_prefix = '.')
+##########
+# Events #
+##########
+
 
 # On Ready Event
 @client.event
 async def on_ready():
-  print(f'Logged in as {client.user}')
+    print(f'Logged in as {client.user}')
 
-# Message Recieved Event
+
+# TODO: Change to db[] eventually
+users = dict()
+
+
+# Listen for Messages
+# All events will be slow because this function is ran first?
 @client.listen('on_message')
 async def on_message(message):
-  if message.author == client.user:
-    return
+    if message.author == client.user:
+        return
 
-  # message.author: MafuMafu Tofu
-  # message.author.name: MafuMafu Tofu
-  # message.author.display_name/.nick = Tim
-  print(f'  {message.author.author}: {message.content}')
+    # Look up author_freq_map of message
+    if message.author in users:
+        # Existing User: Pull up from database
+        author_freq_map = users[message.author]
+    else:
+        # New User: Add to database
+        author_freq_map = FrequencyMap(message.author)
+        users.update({message.author : author_freq_map})
+
+    # Create a word frequency map based on the recieved message
+    word_frequency = generateWordFrequency(author_freq_map, message.content)
+
+    # Update the author_freq_map's word frequency table
+    author_freq_map = word_frequency[0]
+    author_freq_map.wordFreq = word_frequency[1]
+    author_freq_map.sortedKeys = word_frequency[2]
+
+    printWordFreq(author_freq_map)
+    print('\n')
+
+    # DEBUG:
+    # message.author: MafuMafu Tofu
+    # message.author.name: MafuMafu Tofu
+    # message.author.display_name/.nick = Tim
+    # print(f'  {message.author}: {message.content}')
+
 
 # Test Command
 @client.command()
 async def test(ctx):
-  await ctx.send('A command was recieved by the bot')
+    await ctx.send('A command was recieved by the bot')
+
+# @client.command()
+# async def freq(ctx):
+#     printWordFreq()
 
 @client.command()
 async def eli(ctx):
-  await ctx.send('Eli? She\'s only the cutest girlfriend around!')
+    await ctx.send('Eli? She\'s only the cutest girlfriend around!')
+
 
 # Run the Client
 client.run(os.getenv('TOKEN'))
