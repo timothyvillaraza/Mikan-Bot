1579607d584bc9cfe796d836c63a57fe059bcd4d
mafutofu <TimothyVillaraza@outlook.com>
2021-01-02 03:49:48 +0000

    main.py formatting.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from replit import db
 import discord
 from discord.ext import commands
 
-
 # Client Instance: All commands will start with '.'
 client = commands.Bot(command_prefix='.')
 
@@ -19,6 +18,7 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
+
 @client.command()
 async def clear_db(ctx):
     await ctx.send('Clearing all database keys.')
@@ -26,9 +26,8 @@ async def clear_db(ctx):
     for key in db:
         print(f'{key} deleted')
         del db[key]
-    
-    await ctx.send('All keys in the database deleted.')
 
+    await ctx.send('All keys in the database deleted.')
 
 
 # Run the Client
