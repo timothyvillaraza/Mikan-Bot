575c858d0e046e0535929b191b93c9d316b70bf6
mafutofu <TimothyVillaraza@outlook.com>
2021-01-02 08:47:59 +0000

    Cleaned up cog loading code block.

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ from discord.ext import commands
 # Client Instance: All commands will start with '.'
 client = commands.Bot(command_prefix='.', case_insensitive=True)
 
+# Load All Cogs
+print('Loading cogs...')
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        cog_name = filename[:-3]  # File by name without .py extension
+        client.load_extension(f'cogs.{cog_name}')
+        print(f'    {cog_name} loaded')
+print()  # newline
+
 
 # On Ready Event
 @client.event
@@ -13,20 +22,10 @@ async def on_ready():
     print(f'Logged in as {client.user}')
 
 
-# Load All Cogs
-print('Loading cogs...')
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        # Load the file by name (without .py)
-        client.load_extension(f'cogs.{filename[:-3]}')
-        print(f'    {filename[:-3]} loaded')
-print()  # newline
-
-
 @client.command()
 async def clear_db(ctx):
     """
-    
+
     Clears all keys in the repl.it database
 
     """
