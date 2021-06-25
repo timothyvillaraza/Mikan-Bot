d701b215a9271a779b387d1630e208b1b3550e86
mafutofu <61322637+timothyvillaraza@users.noreply.github.com>
2021-06-25 09:13:34 +0000

    Created load and unload commands in main.py Cogs must now be loaded with .loadAllCogs (case insensitive)

--- main.py
+++ main.py
@@ -6,21 +6,61 @@ from discord.ext import commands
 # bot Instance: All commands will start with '.'
 bot = commands.Bot(command_prefix='.', case_insensitive=True)
 
-# Load All Cogs
-print('Loading cogs...')
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        cog_name = filename[:-3]  # File by name without .py extension
-        bot.load_extension(f'cogs.{cog_name}')
-        print(f'    {cog_name} loaded')
-print()  # newline
-
-
 # On Ready Event
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
 
+@bot.command()
+async def load(ctx, extension):
+    cogListString = ''
+    bot.load_extension(f'cogs.{extension}')
+    cogListString+= f'{extension}\n'
+
+    embed_message = discord.Embed(
+        title='Loaded Cogs',
+        color=discord.Color.blue(),
+        description=cogListString
+    )
+
+    await ctx.send(embed=embed_message)
+
+@bot.command()
+async def unload(ctx, extension):
+    cogListString = ''
+    bot.unload_extension(f'cogs.{extension}')
+    cogListString+= f'{extension}\n'
+
+    embed_message = discord.Embed(
+        title='Unloaded Cogs',
+        color=discord.Color.blue(),
+        description=cogListString
+    )
+
+    await ctx.send(embed=embed_message)
+    
+
+# Load All Cogs
+@bot.command()
+async def loadAllCogs(ctx):
+    cogListString = ''
+    
+    print('Loading cogs...')
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            cog_name = filename[:-3]  # File by name without .py extension
+            bot.load_extension(f'cogs.{cog_name}')
+            cogListString+= f'cogs.{cog_name}\n'
+            print(f'    {cog_name} loaded')
+    print()  # newline
+    
+    embed_message = discord.Embed(
+        title='Loaded Cogs',
+        color=discord.Color.blue(),
+        description=cogListString
+    )
+
+    await ctx.send(embed=embed_message)
 
 @bot.command()
 async def clear_db(ctx):
